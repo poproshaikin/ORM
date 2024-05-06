326af805bc4fc6750bab141a094a206814794232
poproshaikin <150174304+poproshaikin@users.noreply.github.com>
2024-05-06 17:58:08 +0200

    Class containing information about column in database

---
+++ Core/Configurations/DbColumnInfo.cs
@@ -0,0 +1,11 @@
+﻿using ORM.Core.Enums;
+
+namespace ORM.Core.Configurations;
+
+public class DbColumnInfo
+{
+    public int Cid { get; set; }
+    public string Name { get; set; }
+    public DbConstraint[] Constraints { get; set; }
+    public string TableName { get; set; }
+}
