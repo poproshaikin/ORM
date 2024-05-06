33e92df6cda4917ff82130a681b31bcb9bd94c8c
poproshaikin <150174304+poproshaikin@users.noreply.github.com>
2024-05-06 17:57:37 +0200

    Static extensions class for collections

---
+++ Core/Extensions/CollectionExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+
+namespace ORM.Core.Extensions;
+
+internal static class CollectionExtensions
+{
+    /// <summary>
+    /// Returns first PropertyInfo that has PrimaryKey constraint attribute or null if not found
+    /// </summary>
+    /// <param name="props">Collection of PropertyInfos</param>
+    public static PropertyInfo? GetPrimaryKey(this IEnumerable<PropertyInfo> props)
+    {
+        return props.FirstOrDefault(p => p.IsPrimaryKey());
+    }
+}
