f2c15357a7731df7d3255c81d27ca2849e4d38a5
poproshaikin <150174304+poproshaikin@users.noreply.github.com>
2024-05-07 09:24:37 +0200

    Processing PostgreSql support

---
+++ PostgreSql/Db/PostgreSqlConnector.cs
@@ -0,0 +1,55 @@
+﻿using System.Data.Common;
+using Npgsql;
+using ORM.Core.Db;
+using ORM.Core.Enums;
+
+namespace ORM.PostgreSql.Db;
+
+internal class PostgreSqlConnector : DbConnector
+{
+    protected override void OpenConnection()
+    {
+        base.DbType = DbType.PostgreSql;
+    }
+
+    protected override void ValidateConnectionString(string connectionString)
+    {
+        base.Connection = new NpgsqlConnection(base.ConnectionString);
+        base.Connection.Open();
+    }
+
+    public override void ExecuteTransaction(Action<DbConnection> trnsAct)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override object? ExecuteScalar(string query)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override void ExecuteNonQuery(DbCommand command)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override void ExecuteNonQuery(string query)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override void ExecuteNonQuery(string query, Action<DbCommand> cmdAct)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override void ExecuteReader(string query, Action<DbDataReader> rdrAct)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override TParam ExecuteReader<TParam>(string query, Func<DbDataReader, TParam> rdrFunc)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ PostgreSql/Db/PostgreSqlQueryBuilder.cs
@@ -0,0 +1,57 @@
+﻿using System.Data.Common;
+using ORM.Core.Configurations;
+using ORM.Core.Db;
+
+namespace ORM.PostgreSql.Db;
+
+internal class PostgreSqlQueryBuilder : DbQueryBuilder
+{
+    public PostgreSqlQueryBuilder(DbConnector connector) : base(connector)
+    {
+    }
+
+    public override DbCommand CreateTableIfNotExists(TableConfig config)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override TEntity? GetEntity<TEntity>(string tableName, int id) where TEntity : class
+    {
+        throw new NotImplementedException();
+    }
+
+    public override IEnumerable<TEntity?> GetEntities<TEntity>(string tableName) where TEntity : class
+    {
+        throw new NotImplementedException();
+    }
+
+    public override DbCommand RemoveEntity(string tableName, int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override DbCommand AddEntity<TEntity>(string tableName, TEntity entity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override IEnumerable<DbCommand> UpdateEntity<TEntity>(string tableName, TEntity oldEntity, TEntity newEntity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override string GetPrimaryKeyColumnName(string tableName)
+    {
+        throw new NotImplementedException();
+    }
+
+    protected override string GetObjectJson(string tableName, int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override bool ExistsTable(string tableName)
+    {
+        throw new NotImplementedException();
+    }
+}
